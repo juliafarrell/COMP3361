d503502b3efb99f590221d2489651705fc24bd1d
juliafarrell <juliafarrell1.618@gmail.com>
2018-04-15 23:24:44 -0600

    framed methods of memalloc class

--- Lab3/MemoryAllocator.cpp
+++ Lab3/MemoryAllocator.cpp
@@ -8,11 +8,43 @@
 #include "MemoryAllocator.h"
 
 MemoryAllocator::MemoryAllocator() {
+    throw std::runtime_error("need to define memory size");
+}
+
+MemoryAllocator::MemoryAllocator(uint8_t num_pages) {
+    // set memory to proper size, assuming page size of 16^4 bytes
+    mem.resize(num_pages * 65536);
+    mem[pf_total] = num_pages;
+    // first page reserved for memory allocator
+    mem[pf_free] = num_pages - 1;
+    // ?? change to 32 bit thing??
+    mem[free_list_head] = 0x10000;
+    
+    // build linked list of free pages
+    // info of next page stored in first 4 bytes
+    for (int i = 1; i < num_pages; i++) {
+        // step 1: get next page value in 32 bytes as an array of 8 bytes
+        
+        // step 2: put that array into the memory vector
+    }
 }
 
 MemoryAllocator::MemoryAllocator(const MemoryAllocator& orig) {
+    throw std::runtime_error("no copy constructor");
 }
 
 MemoryAllocator::~MemoryAllocator() {
+    std::cout << "empty destructor ~MemoryAllocator was called";
 }
 
+bool AllocatePageFrames(uint32_t count, std::vector<uint32_t> &page_frames) {
+    
+}
+
+bool FreePageFrames(uint32_t count, std::vector<uint32_t> &page_frames) {
+    
+}
+
+uint32_t get_page_frames_free() {
+    
+}
--- Lab3/MemoryAllocator.h
+++ Lab3/MemoryAllocator.h
@@ -8,13 +8,28 @@
 #ifndef MEMORYALLOCATOR_H
 #define MEMORYALLOCATOR_H
 
+#include <stdexcept>
+#include <stdint.h>
+#include <vector>
+
 class MemoryAllocator {
 public:
+    std::vector<uint8_t> mem;
     MemoryAllocator();
+    MemoryAllocator(uint8_t num_pages);
     MemoryAllocator(const MemoryAllocator& orig);
     virtual ~MemoryAllocator();
+    bool AllocatePageFrames(uint32_t count, std::vector<uint32_t> &page_frames); 
+    bool FreePageFrames(uint32_t count, std::vector<uint32_t> &page_frames);
+    uint32_t get_page_frames_free() const;
 private:
-
+    // Index of total number of page frames in mem vector
+    // Should hold 32 bit value, so 4 * 8 bits.
+    static const uint8_t pf_total = 0;
+    // Index of number of free page frames in mem vector
+    static const uint8_t pf_free = 4;
+    // Index of address for head of the free list in mem vector
+    static const uint8_t free_list_head = 8;
 };
 
 #endif /* MEMORYALLOCATOR_H */
